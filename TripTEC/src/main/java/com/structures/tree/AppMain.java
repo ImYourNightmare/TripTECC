aec21a61eb783e32f6390099e7fa278375d1ee7c
Marcos Méndez Hidalgo <marcosmh0199@gmail.com>
2018-11-25 01:36:53 -0600

    Update AppMain.java

--- TripTEC/src/main/java/com/structures/tree/AppMain.java
+++ TripTEC/src/main/java/com/structures/tree/AppMain.java
@@ -5,7 +5,14 @@ import com.logic.objects.*;
 public class AppMain {
 	public static void main(String[] args) {
 		BinarySearchTree<Client> bst = new BinarySearchTree<Client>();
-
+		System.out.println("PROBANDO CON CLIENTES");
+		Client cliente1 = new Client("Marcos","5-10",13,"201520");
+		Client cliente2 = new Client("Bryan","6",3,"20");
+		Client cliente3 = new Client("Manuel","3",65,"20");
+		Client cliente4 = new Client("Melina","5",14,"20");
+		bst.insertarNodo(cliente1);
+		bst.insertarNodo(cliente2);
+		bst.insertarNodo(cliente3);
 		bst.insertarNodo(cliente4);
 		//Client cliente5 = bst.searchNode(cliente1); // Se extrae un cliente del árbol para clonar un usuario
 
